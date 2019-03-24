b7a9170c003b4727002bbec7685ab61a84f3ab1e
“[Rachel <rachelduan510@163.com>
2019-03-24 18:30:52 +0000

    changing half of the formatting

--- __init__.py
+++ __init__.py
@@ -14,61 +14,61 @@ def startgame():
 def playagain():
 	return render_template("index.html") 
 
-@app.route("/1-nochallenge.html")
+@app.route("/1-nochallenge")
 def nochallenge():
 	global endgame
 	endgame = "Does honour mean nothing to you?"
 	return render_template("1-nochallenge.html")
 
-@app.route("/1-yeschallenge.html")
+@app.route("/1-yeschallenge")
 def yeschallenge():
 	return render_template("1-yeschallenge.html")
 
-@app.route("/2-left.html")
+@app.route("/2-left")
 def left():
 	global endgame
 	endgame = "Oh no, you drop through the crack and get injured!"
 	return render_template("2-left.html")
 
-@app.route("/2-right.html")
+@app.route("/2-right")
 def right():
 	global endgame
 	endgame = "Turns out you have terrible night vision. You got injured."
 	return render_template("2-right.html")
 
-@app.route("/3-torch.html")
+@app.route("/3-torch")
 def torch():
 	global endgame
 	endgame = "The bats overwhelmed you!"
 	return render_template("3-torch.html")
 
-@app.route("/4-founddragon.html")
+@app.route("/4-founddragon")
 def founddragon():
 	global endgame
 	endgame = "You are obviously not strong enough to defeat the dragon!"
 	return render_template("4-founddragon.html")
 
-@app.route("/5-foundprincess.html")
+@app.route("/5-foundprincess")
 def foundprincess():
 	return render_template("5-foundprincess.html")
 
-@app.route("/6-goafterdragon.html")
+@app.route("/6-goafterdragon")
 def goafterdragon():
 	global endgame
 	endgame = "You are obviously not strong enough to defeat the dragon!"
 	return render_template("6-goafterdragon.html")
 
-@app.route("/6-saveprincess.html")
+@app.route("/6-saveprincess")
 def saveprincess():
 	global endgame
 	endgame = "You are obviously not strong enough to defeat the dragon!"
 	return render_template("6-saveprincess.html")
 
-@app.route("/7-frienddragon.html")
+@app.route("/7-frienddragon")
 def frienddragon():
 	return render_template("7-frienddragon.html")
 
-@app.route("/7-winner.html")
+@app.route("/7-winner")
 def winner():
 	return render_template("7-winner.html")
 
