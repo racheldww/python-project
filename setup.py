f78f9762f17c5c989610a2172f1097e7126993ef
“[Rachel <rachelduan510@163.com>
2019-03-25 19:11:09 +0000

    updating

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 # import the Flask class from the flask module
 from flask import Flask, render_template, redirect, url_for, request
-import requests
 
 # create the application object
 app = Flask("MyApp")
